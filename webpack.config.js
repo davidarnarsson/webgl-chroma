ead1ea2c9f741cd92ef0da3d616f0c83a28b2acf
Davíð Arnarsson <davidarnarsson@gmail.com>
2017-05-07 14:50:43 +0000

    oops

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "react-hot-loader/patch",
     // activate HMR for React
 
-    "webpack-dev-server/client?https://89.160.199.239:8080",
+    "webpack-dev-server/client?https://localhost:8080",
     // bundle the client for webpack-dev-server
     // and connect to the provided endpoint
 
